145c15dc06e55cb9a0ba2bcf513a522525e2e490
hyhy8181994 <hyhy8181994@gmail.com>
2021-07-20 22:49:14 -0400

    Add files via upload

--- get_top_transcript_abundance_gene.py
+++ get_top_transcript_abundance_gene.py
@@ -1,10 +1,8 @@
 import pandas as pd
+import pickle as pk
 
 def get_top_gene(infile,annotation):
-    ann_df = pd.read_table(annotation)
-    for index,row in ann_df.iterrows():
-        id = row["contig_id"]
-
+    ann = pk.load(open(annotation,"rb"))
     df = pd.read_table(infile)
     sorted_df = df.sort_values(["Average_tpm"],ascending=False)
     tpm_only_df = sorted_df[["Geneid","Average_tpm"]]
@@ -15,8 +13,8 @@ if __name__ == '__main__':
     
     alfalfa_gene = "./alfalfa_triplicate_salmon_tpm.txt"
     clover_gene = "./clover_triplicate_salmon_tpm.txt"
-    alfalfa_ann = "./blast/alfalfa_filtered_annotations_1.tsv"
-    clover_ann = "./blast/clover_filtered_annotations_1.tsv"
+    alfalfa_ann = "./blast/alfalfa_functional_annotations.dat"
+    clover_ann = "./blast/clover_functional_annotations.dat"
     get_top_gene(alfalfa_gene,alfalfa_ann)
     get_top_gene(clover_gene,clover_ann)
     
